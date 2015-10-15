86b7b53a78b1ca051a01674bceaec923db431a16
thisdotrob <thisdotrob@gmail.com>
2015-10-15 12:14:27 +0100

    Ch 15 stuff

--- lib/docking_station.rb
+++ lib/docking_station.rb
@@ -7,13 +7,23 @@ class DockingStation
   # attr_reader :bikes
 
   def release_bike
-    raise 'shit there is no bike!' unless @bikes.length > 0
+    raise 'shit there is no bike!' if empty?
     @bikes.pop
   end
 
   def dock(bike)
-    raise 'docking station is full' if @bikes.length == 20
+    raise 'docking station is full' if full?
     @bikes << bike
+  end
+
+  private
+
+  def full?
+    @bikes.length == 20 ? true : false
+  end
+
+  def empty?
+    @bikes.empty? ? true : false
   end
 
 end
