078589e9ead5677a4c787229f886253334509390
Ivan Sathianathan <ivan.sathianathan@gmail.com>
2015-10-15 17:25:13 +0100

    up to chapter 20

--- spec/docking_station_spec.rb
+++ spec/docking_station_spec.rb
@@ -2,12 +2,16 @@ require 'docking_station.rb'
 require 'bike.rb'
 
   describe DockingStation do
+
     it { is_expected.to respond_to :release_bike }
 
     it { is_expected.to respond_to(:dock).with(1).argument }
 
+    let(:bike) { double :bike}
+
     it "creates a bike when release_bike is called" do
-      subject.dock(Bike.new)
+      allow(bike).to receive(:working).and_return(true)
+      subject.dock(bike)
       bike = subject.release_bike
       expect(bike.working).to eq true
     end
@@ -17,20 +21,25 @@ require 'bike.rb'
     end
 
     it "raises error when a docking station is full" do
-      DockingStation::DEFAULT_CAPACITY.times { subject.dock(Bike.new) }
-      expect {subject.dock(Bike.new)}.to raise_error('docking station is full')
+      DockingStation::DEFAULT_CAPACITY.times { subject.dock(bike) }
+      expect {subject.dock(bike)}.to raise_error('docking station is full')
     end
 
     it "does not release broken bikes" do
-      bike = Bike.new
+      allow(bike).to receive(:break)
+      allow(bike).to receive(:working).and_return false
       bike.break
       subject.dock(bike)
       expect { subject.release_bike }.to raise_error('no working bikes available')
     end
 
+    let(:bike1) { double :bike1}
+    let(:bike2) { double :bike2}
+
     it "releases a working bike when there are broken bikes present" do
-      bike1 = Bike.new
-      bike2 = Bike.new
+      allow(bike1).to receive(:working).and_return true
+      allow(bike2).to receive(:break)
+      allow(bike2).to receive(:working).and_return false
       bike2.break
       subject.dock(bike1)
       subject.dock(bike2)
@@ -38,7 +47,10 @@ require 'bike.rb'
     end
 
     it "user reports working state of bike" do
-      bike = Bike.new
+      bike = double("bike", :break => false)
+      bike = double("bike", :working => false)
+      allow(bike).to receive(:break)
+      allow(bike).to receive(:working).and_return false
       bike.break
       subject.dock(bike)
       expect(subject.bikes.last.working).to eq false
